8aeadbe3f3878c46dbb6234d5ce21dbcc68286e0
ParallelScore Mac - 2021 (1) <ogunye4pao@gmail.com>
2021-03-01 03:54:14 +0100

    added git ignore

--- smoothtalk/config/config.js
+++ smoothtalk/config/config.js
@@ -7,7 +7,5 @@ const environment = process.env.NODE_ENV || 'development';
 const environmentConfig = config[environment];
 const finalConfig = _.merge(defaultConfig, environmentConfig);
 
-// as a best practice
-// all global variables should be referenced via global. syntax
-// and their names should always begin with g
+
 global.gConfig = finalConfig;
