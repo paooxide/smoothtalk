07396ab3bc75270622784fbc2b85173da120e169
ParallelScore Mac - 2021 (1) <ogunye4pao@gmail.com>
2021-03-03 00:39:31 +0100

    bot update

--- smoothtalk/index.js
+++ smoothtalk/index.js
@@ -11,12 +11,29 @@ const app = new App({
 /* Add functionality here */
 
 
-// Listens to incoming messages that contain "hello"
 app.message('hello', async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
-    await say(`Hey there <@${message.user}>!`);
-  });
-  
+    await say({
+        blocks: [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `Hey there <@${message.user}>!`
+                },
+                "accessory": {
+                    "type": "button",
+                    "text": {
+                        "type": "plain_text",
+                        "text": "Click Me"
+                    },
+                    "action_id": "button_click"
+                }
+            }
+        ],
+        text: `Hey there <@${message.user}>!`
+    });
+});
 
 (async () => {
     // Start the app
