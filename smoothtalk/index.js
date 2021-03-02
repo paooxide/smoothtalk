b45f1a07a51b45401ab5aaf402ce5fac63b01341
ParallelScore Mac - 2021 (1) <ogunye4pao@gmail.com>
2021-03-03 00:45:48 +0100

    bot update

--- smoothtalk/index.js
+++ smoothtalk/index.js
@@ -35,6 +35,14 @@ app.message('hello', async ({ message, say }) => {
     });
 });
 
+
+app.action('button_click', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    await say(`<@${body.user.id}> clicked the button`);
+  });
+
+  
 (async () => {
     // Start the app
     await app.start(process.env.PORT || 3000);
