2d13a6ad0a3ec8d2833065453a90ec0752f87299
ParallelScore Mac - 2021 (1) <ogunye4pao@gmail.com>
2021-03-03 01:39:50 +0100

    bot update

--- smoothtalk/index.js
+++ smoothtalk/index.js
@@ -15,7 +15,7 @@ const app = new App({
 
 app.message('hello', async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
-    
+
     await say(`Welcome!`);
     await say({
         blocks: [
@@ -72,38 +72,212 @@ app.message('hello', async ({ message, say }) => {
 
 
 
-app.message('hellott', async ({ message, say }) => {
-    // say() sends a message to the channel where the event was triggered
-    
-    await say(`Welcome! How are you doing ?`);
+// app.message('hellott', async ({ message, say }) => {
+//     // say() sends a message to the channel where the event was triggered
+
+//     await say(`Welcome! How are you doing ?`);
+//     await say({
+//         blocks: [
+//             {
+//                 "type": "section",
+//                 "text": {
+//                     "type": "mrkdwn",
+//                     "text": `Hey there <@${message.user}>!`
+//                 },
+//                 "accessory": {
+//                     "type": "button",
+//                     "text": {
+//                         "type": "plain_text",
+//                         "text": "Click Me"
+//                     },
+//                     "action_id": "button_click"
+//                 }
+//             }
+//         ],
+//         text: `Hey there <@${message.user}>!`
+//     });
+// });
+
+
+
+
+app.action('static_select-action', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    console.log(body);
     await say({
         blocks: [
             {
                 "type": "section",
                 "text": {
                     "type": "mrkdwn",
-                    "text": `Hey there <@${message.user}>!`
+                    "text": "when are you free this week for a walk?"
                 },
                 "accessory": {
-                    "type": "button",
-                    "text": {
+                    "type": "datepicker",
+                    "initial_date": "2021-03-03",
+                    "placeholder": {
                         "type": "plain_text",
-                        "text": "Click Me"
+                        "text": "Select a date",
+                        "emoji": true
                     },
-                    "action_id": "button_click"
+                    "action_id": "datepicker-action"
                 }
             }
+        ],
+        text: `Hey there <@${message.user}>!`
+    });
+
+});
+
+
+
+app.action('datepicker-action', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    console.log(body);
+    await say(`<@${body.user.id}> Please select time `);
+    await say({
+        blocks: [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": " "
+                },
+                "accessory": {
+                    "type": "timepicker",
+                    "initial_time": "12:30",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select time",
+                        "emoji": true
+                    },
+                    "action_id": "timepicker-action"
+                }
+            }
+
+        ],
+        text: `Hey there <@${message.user}>!`
+    });
+
+});
+
+
+
+app.action('timepicker-action', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    console.log(body);
+    await say({
+        blocks: [
+            {
+                "type": "section",
+                "block_id": "section678",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "What are your favorite hobbies"
+                },
+                "accessory": {
+                    "action_id": "favoriteHobbies",
+                    "type": "multi_static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select items"
+                    },
+                    "options": [
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Football"
+                            },
+                            "value": "value-0"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Music"
+                            },
+                            "value": "value-1"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Sleep"
+                            },
+                            "value": "value-2"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Movies"
+                            },
+                            "value": "value-3"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Basketball"
+                            },
+                            "value": "value-4"
+                        }
+                    ]
+                }
+            }
+
         ],
         text: `Hey there <@${message.user}>!`
     });
 });
 
 
+
+
+app.action('favoriteHobbies', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    console.log(body);
+
+    await say({
+        blocks: [
+            {
+                "type": "input",
+                "block_id": "input123",
+                "label": {
+                  "type": "plain_text",
+                  "text": "Label of input"
+                },
+                "element": {
+                  "type": "plain_text_input",
+                  "action_id": "freeText",
+                  "placeholder": {
+                    "type": "plain_text",
+                    "text": "Enter some plain text"
+                  }
+                }
+              }
+
+        ],
+        text: `Hey there <@${message.user}>!`
+    });
+});
+
+
+
+app.action('freeText', async ({ body, ack, say }) => {
+    // Acknowledge the action
+    await ack();
+    console.log(body);
+    await say(`Thank you <@${body.user.id}> `);
+});
+
+
+
 app.action('button_click', async ({ body, ack, say }) => {
     // Acknowledge the action
     await ack();
     await say(`<@${body.user.id}> clicked the button`);
-  });
+});
 
 
 (async () => {
