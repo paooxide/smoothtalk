164b6054e1132b386814a64ff29694015c368b38
ParallelScore Mac - 2021 (1) <ogunye4pao@gmail.com>
2021-03-03 01:02:13 +0100

    bot update

--- smoothtalk/index.js
+++ smoothtalk/index.js
@@ -11,8 +11,71 @@ const app = new App({
 /* Add functionality here */
 
 
-app.message('hello', async ({ message, say }) => {
+
+
+app.message('hellott', async ({ message, say }) => {
     // say() sends a message to the channel where the event was triggered
+    
+    await say(`Welcome!`);
+    await say({
+        blocks: [
+            {
+                "type": "divider"
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "How are you doing?"
+                },
+                "accessory": {
+                    "type": "static_select",
+                    "placeholder": {
+                        "type": "plain_text",
+                        "text": "Select an item",
+                        "emoji": true
+                    },
+                    "options": [
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Doing Well",
+                                "emoji": true
+                            },
+                            "value": "value-0"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Neutral",
+                                "emoji": true
+                            },
+                            "value": "value-1"
+                        },
+                        {
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Feeling Lucky",
+                                "emoji": true
+                            },
+                            "value": "value-2"
+                        }
+                    ],
+                    "action_id": "static_select-action"
+                }
+            }
+        ],
+        text: `Hey there <@${message.user}>!`
+    });
+});
+
+
+
+
+app.message('hellott', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    
+    await say(`Welcome! How are you doing ?`);
     await say({
         blocks: [
             {
@@ -42,7 +105,7 @@ app.action('button_click', async ({ body, ack, say }) => {
     await say(`<@${body.user.id}> clicked the button`);
   });
 
-  
+
 (async () => {
     // Start the app
     await app.start(process.env.PORT || 3000);
